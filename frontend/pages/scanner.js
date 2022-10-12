410b0bbe026df9fe7c26afcc5c7075a4c10f3955
ewinata <ermanowinatawml@gmail.com>
2022-10-13 01:16:28 +0700

    cleaned up pages directory

--- frontend/pages/scanner.js
+++
@@ -1,9 +0,0 @@
-// layout for this page
-import Admin from "layouts/Admin.js";
-import Scanner from "components/PageComponents/ScannerDashboard";
-
-const ScannerPage = () => <Scanner />
-
-ScannerPage.layout = Admin;
-
-export default ScannerPage;

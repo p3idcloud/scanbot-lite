a55bfdbeba2eacfa55ed9e02c2eda48088480eab
tigaron <suputra@mail.com>
2022-10-21 01:49:07 +0800

    feat: endpoint to send pdf file to email

--- backend/src/controllers/storage.controller.js
+++ backend/src/controllers/storage.controller.js
@@ -1,6 +1,7 @@
 const AccountService = require('../services/account');
 const Minio = require('minio');
 const PDFMerger = require('pdf-merger-js');
+const nodemailer = require("nodemailer");
 
 const minioClient = new Minio.Client({
   endPoint: process.env.AWS_ENDPOINT,
@@ -35,7 +36,7 @@ function dataStreamToBuffer(stream) {
   });
 }
 
-exports.generatePdfDocument = async (req, res) => {
+exports.mergePdfDocument = async (req, res) => {
   try {
     const merger = new PDFMerger();
 
@@ -53,6 +54,49 @@ exports.generatePdfDocument = async (req, res) => {
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=ScanBot.pdf');
     res.end(mergedPdf);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.sendPdfDocument = async (req, res) => {
+  try {
+    const merger = new PDFMerger();
+
+    const { imageURI, recipient } = req.body || {};
+    if (!imageURI?.length) return res.status(400).send('No image URI provided');
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(String(recipient))) return res.status(400).send('Invalid recipient email');
+
+    const account = await AccountService.getAccountFromId(req.twain.principalId);
+
+    for await (const uri of imageURI) {
+      const dataStream = await minioClient.getObject(account.id, uri);
+      await merger.add(Buffer.from(await dataStreamToBuffer(dataStream)));
+    }
+
+    const mergedPdf = await merger.saveAsBuffer();
+
+    // TODO change to a valid email account and transporter
+    const testAccount = await nodemailer.createTestAccount();
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Scanbot Lite" <scanbot-lite@lyrid.io>',
+      to: String(recipient),
+      subject: "Test",
+      text: "This is just a test",
+      attachments: [{ filename: 'ScanBot.pdf', content: mergedPdf }],
+    });
+
+    return res.status(200).json({ message: 'Email sent', preview_url: nodemailer.getTestMessageUrl(info) });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- backend/src/routes/storage.js
+++ backend/src/routes/storage.js
@@ -1,5 +1,5 @@
 'use strict';
-const { generatePdfDocument, getFileFromURI } = require('../controllers/storage.controller');
+const { mergePdfDocument, sendPdfDocument, getFileFromURI } = require('../controllers/storage.controller');
 const express = require('express');
 const router = express.Router();
 
@@ -34,7 +34,7 @@ router.get('/:uri*', getFileFromURI);
 
 /**
  * @swagger
- * /api/storage/:
+ * /api/storage/mergepdf:
  *  post:
  *    summary: Generate merged PDF document from imageURI
  *    description: imageURI can be found in scan history job's detail
@@ -63,6 +63,37 @@ router.get('/:uri*', getFileFromURI);
  *      500:
  *        description: Error occured
  */
-router.post('/', generatePdfDocument);
+router.post('/mergepdf', mergePdfDocument);
+
+/**
+ * @swagger
+ * /api/storage/sendpdf:
+ *  post:
+ *    summary: Generate merged PDF document from imageURI
+ *    description: imageURI can be found in scan history job's detail
+ *    security:
+ *    - bearerAuth: []
+ *    tags:
+ *      - Storage
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              recipient:
+ *                type: string
+ *                example: "scanbot@test.com"
+ *              imageURI:
+ *                type: array
+ *                example: ["396cd224-4d99-4888-9a12-22b2f83e7f8d/scannedDocuments/3d7fb595-c06c-43e7-bded-46a7cced9978/cc7ae714-0b51-4c32-a372-bf50cd6ee2f6","396cd224-4d99-4888-9a12-22b2f83e7f8d/scannedDocuments/3d7fb595-c06c-43e7-bded-46a7cced9978/415e88a1-4e5b-4247-9aa7-adfca578aa28","396cd224-4d99-4888-9a12-22b2f83e7f8d/scannedDocuments/3d7fb595-c06c-43e7-bded-46a7cced9978/bf8d18b1-66c0-4271-b31c-efd7c2d22e1f"]
+ *    responses:
+ *      200:
+ *        description: Email sent
+ *      500:
+ *        description: Error occured
+ */
+router.post('/sendpdf', sendPdfDocument);
 
 module.exports = router;

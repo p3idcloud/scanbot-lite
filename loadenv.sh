6cae6d8d1c101334751b30716ed5fdbd015111e5
ewinata <ermanowinatawml@gmail.com>
2022-10-26 00:35:33 +0700

    fixed bash script

--- loadenv.sh
+++ loadenv.sh
@@ -32,6 +32,20 @@ KEYCLOAK_PW="KEYCLOAK_PW"
 ENV_LIST=()
 VALUE_LIST=()
 
+DEV_ENV=()
+DEV_VALUE=()
+
+PROD_ENV=()
+PROD_VALUE=()
+
+PROD_FRONTEND_ENV=()
+PROD_FRONTEND_VALUE=()
+
+DEV_FRONTEND_ENV=()
+DEV_FRONTEND_VALUE=()
+
+read -p "Enter your ip for env: " ip
+
 write_to_env () {
     filename_dev='backend/.env.development'
     filename_prod='backend/.env.production'
@@ -39,13 +53,22 @@ write_to_env () {
     # dev
     while read line
     do
-        for i in "${!ENV_LIST[@]}";
+        for i in "${!DEV_FRONTEND_ENV[@]}";
         do
-            if [[ $line =~ ^${ENV_LIST[$i]}* ]];
+            if [[ $line =~ ^${DEV_FRONTEND_ENV[$i]}* ]];
             then
                 line="${line%%=*}=${VALUE_LIST[$i]}"
             fi
         done
+        
+        for i in "${!DEV_ENV[@]}";
+        do
+            if [[ $line =~ ^${DEV_ENV[$i]}* ]];
+            then
+                line="${line%%=*}=${DEV_VALUE[$i]}"
+            fi
+        done
+
         echo $line >> 'backend/.env.development.temp'
     done < $filename_dev
 
@@ -59,14 +82,72 @@ write_to_env () {
                 line="${line%%=*}=${VALUE_LIST[$i]}"
             fi
         done
+
+        for i in "${!PROD_ENV[@]}";
+        do
+            if [[ $line =~ ^${PROD_ENV[$i]}* ]];
+            then
+                line="${line%%=*}=${PROD_VALUE[$i]}"
+            fi
+        done
+
         echo $line >> 'backend/.env.production.temp'
     done < $filename_prod
     
-    cp -fr backend/.env.production.temp backend/.env.development
+    cp -fr backend/.env.development.temp backend/.env.development
     cp -fr backend/.env.production.temp backend/.env.production
     rm backend/.env.production.temp
     rm backend/.env.development.temp
+
+    
+    filename_dev='frontend/.env.development'
+    filename_prod='frontend/.env.production'
+    
+    # dev
+    while read line
+    do
+        for i in "${!DEV_FRONTEND_ENV[@]}";
+        do
+            if [[ $line =~ ^${DEV_FRONTEND_ENV[$i]}* ]];
+            then
+                line="${line%%=*}=${DEV_FRONTEND_VALUE[$i]}"
+            fi
+        done
+        echo $line >> 'frontend/.env.development.temp'
+    done < $filename_dev
+
+    # prod
+    while read line
+    do
+        for i in "${!PROD_FRONTEND_ENV[@]}";
+        do
+            if [[ $line =~ ^${PROD_FRONTEND_ENV[$i]}* ]];
+            then
+                line="${line%%=*}=${PROD_FRONTEND_VALUE[$i]}"
+            fi
+        done
+        echo $line >> 'frontend/.env.production.temp'
+    done < $filename_prod
+    
+    cp -fr frontend/.env.development.temp frontend/.env.development
+    cp -fr frontend/.env.production.temp frontend/.env.production
+    rm frontend/.env.production.temp
+    rm frontend/.env.development.temp
     return
+}
+
+add_to_dev () {
+    # $1 for env key $2 for value
+    # Checks if key exists in env and appends it
+    DEV_ENV+=($1)
+    DEV_VALUE+=($2)
+}
+
+add_to_prod () {
+    # $1 for env key $2 for value
+    # Checks if key exists in env and appends it
+    PROD_ENV+=($1)
+    PROD_VALUE+=($2)
 }
 
 add_to_env () {
@@ -76,6 +157,20 @@ add_to_env () {
     VALUE_LIST+=($2)
 }
 
+add_to_dev_frontend () {
+    # $1 for env key $2 for value
+    # Checks if key exists in env and appends it
+    DEV_FRONTEND_ENV+=($1)
+    DEV_FRONTEND_VALUE+=($2)
+}
+
+add_to_prod_frontend () {
+    # $1 for env key $2 for value
+    # Checks if key exists in env and appends it
+    PROD_FRONTEND_ENV+=($1)
+    PROD_FRONTEND_VALUE+=($2)
+}
+
 CURRENTVAR=""
 MONGOURL=""
 
@@ -83,6 +178,14 @@ MONGOURL=""
 add_to_env "AWS_REGION" "us-east-1"
 add_to_env "MINIO_PORT" "443"
 add_to_env "MINIO_USE_SSL" "true"
+add_to_dev "BASE_URL" "http://$ip/"
+add_to_dev "FRONTEND_URL" "http://$ip:3000/"
+add_to_prod "BASE_URL" "https://$ip/"
+add_to_prod "FRONTEND_URL" "https://$ip:3000/"
+add_to_dev_frontend "BASE_URL" "http://$ip:3000/"
+add_to_dev_frontend "BACKEND_URL" "http://$ip/"
+add_to_prod_frontend "BASE_URL" "https://$ip:3000/"
+add_to_prod_frontend "BACKEND_URL" "https://$ip/"
 
 file="$1"
 if [[ "$file" -eq "" ]]
@@ -163,7 +266,6 @@ do
             CURRENTVAR=""
             ;;
         $AWS_ENDPOINT)
-            line=${line%?}
             add_to_env $AWS_ENDPOINT $line
             CURRENTVAR=""
             ;;
@@ -193,10 +295,10 @@ do
         *"$JWT_SECRET_JSON"*)
             CURRENTVAR=$JWT_SECRET
             ;;
-        *"$MINIO_KEY_JSON"*)
+        *"$MINIO_SECRET_JSON"*)
             CURRENTVAR=$AWS_ACCESS_KEY
             ;;
-        *"$MINIO_SECRET_JSON"*)
+        *"$MINIO_KEY_JSON"*)
             CURRENTVAR=$AWS_SECRET_KEY
             ;;
         *"$MINIO_CONSOLE_URL_JSON"*)
@@ -209,3 +311,5 @@ do
 done
 
 write_to_env
+add_to_dev
+add_to_prod

ee912896fa022e528144f7fd6ee6a24e67806967
tigaron <suputra@mail.com>
2022-11-03 08:39:28 +0800

    refactor: added isEmpty check

--- backend/src/controllers/exercise.controller.js
+++ backend/src/controllers/exercise.controller.js
@@ -3,24 +3,25 @@ const { getAccountFromId } = require('../services/account.js');
 const { getJobFromId } = require('../services/job.js');
 const { getQueueFromId } = require('../services/queue.js');
 const { getScannerHistoryFromId } = require('../services/scannerhistory.js');
+const isEmpty = require('../utils/isEmpty.js');
 
 async function getImageURIsFromScannerHistory(req, res, next) {
   try {
     const { principalId } = req.twain || {};
-    if (!principalId?.length) throw new Error(`Please log in to your account`, { cause: 401 });
+    if (isEmpty(principalId)) throw new Error(`Please log in to your account`, { cause: 401 });
 
     const { id } = await getAccountFromId(principalId) || {};
-    if (!id?.length) throw new Error(`Account does not exist`, { cause: 404 });
+    if (isEmpty(id)) throw new Error(`Account does not exist`, { cause: 404 });
 
     const { accountId, queueId } = await getScannerHistoryFromId(req.params['scannerHistoryId']) || {};
-    if (!accountId?.length || !queueId?.length) throw new Error(`Scanner history does not exist`, { cause: 404 });
-    if (accountId !== id) throw new Error(`You have no permission to access this information`, { cause: 401 });
+    if (isEmpty(accountId) || isEmpty(accountId)) throw new Error(`Scanner history does not exist`, { cause: 404 });
+    if (accountId !== id) throw new Error(`You have no permission to access this information`, { cause: 403 });
 
     const { jobId } = await getQueueFromId(queueId) || {};
-    if (!jobId?.length) throw new Error(`No data found`, { cause: 404 });
+    if (isEmpty(jobId)) throw new Error(`No data found`, { cause: 404 });
 
     const { imageURI } = await getJobFromId(jobId) || {};
-    if (!imageURI?.length) throw new Error(`No data found`, { cause: 404 });
+    if (isEmpty(imageURI)) throw new Error(`No data found`, { cause: 404 });
 
     req.imageURI = imageURI;
     next();
@@ -32,10 +33,10 @@ async function getImageURIsFromScannerHistory(req, res, next) {
 async function mergePDFFromImageURIs(req, res, next) {
   try {
     const { principalId } = req.twain || {};
-    if (!principalId?.length) throw new Error(`Please log in to your account`, { cause: 401 });
+    if (isEmpty(principalId)) throw new Error(`Please log in to your account`, { cause: 401 });
 
     const { id } = await getAccountFromId(principalId) || {};
-    if (!id?.length) throw new Error(`Account does not exist`, { cause: 404 });
+    if (isEmpty(id)) throw new Error(`Account does not exist`, { cause: 404 });
 
     const [mergedPDF, error] = await ExerciseService.mergePDFFromImageURIs(id, req.imageURI);
     if (error) throw new Error(`Unable to merge pdf documents at this moment`, { cause: 408 });

052e0695d27e8b016740e395315b7935447af989
coolize <cool_ize@hotmail.com>
2024-11-14 19:41:08 +0700

    Delete backend/install.sh

--- backend/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v apt-get >/dev/null; then
-  apt-get update
-  apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
-else
-  echo "Do not pre-build"
-fi
